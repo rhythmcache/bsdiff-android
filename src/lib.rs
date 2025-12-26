6f79f8dde2ef16463af7e014532bddc3df715f7c
rhythmcache <153998419+rhythmcache@users.noreply.github.com>
2025-12-26 10:56:01 +0530

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,13 @@
 mod diff;
 mod patch;
 mod bsdf2;
+mod bsdf2_writer;
 
-pub use diff::diff;
+pub use diff::{diff, diff_bsdiff40, diff_bsdf2, diff_bsdf2_uniform};
 pub use patch::patch;
-pub use bsdf2::{patch_bsdf2, CompressionAlgorithm};
+pub use bsdf2::{patch_bsdf2, parse_bsdf2_header};
+
+pub use bsdf2_writer::{CompressionAlgorithm, ControlEntry, Bsdf2Writer};
 
 pub use patch::patch as apply_patch;
 pub use bsdf2::patch_bsdf2 as apply_bsdf2_patch;
